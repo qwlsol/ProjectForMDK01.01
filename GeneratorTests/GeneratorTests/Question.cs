9df782f2fab22df817f592565c67d1dccff757b0
guzer <guzer@DESKTOP-K1P33T2>
2026-06-13 19:45:02 +0300

    дальше были созданы переменные

--- GeneratorTests/GeneratorTests/Question.cs
+++ GeneratorTests/GeneratorTests/Question.cs
@@ -14,5 +14,12 @@ namespace GeneratorTests
     }
     public class Question
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Topic { get; set; }
+        public string Difficulty { get; set; }
+        public string CorrectAnswer { get; set; }
+        public List<string> Options { get; set; }
+        public QuestionType Type { get; set; }
     }
 }
