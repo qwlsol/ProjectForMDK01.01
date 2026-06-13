b14766ede87acf63171c59d4f1709eb007ea2686
guzer <guzer@DESKTOP-K1P33T2>
2026-06-13 19:55:34 +0300

    был создан класс Генератор Тестов и внего были добавлены перемнные( список вопросов, рандомайзер и id)

---
+++ GeneratorTests/GeneratorTests/TestGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTests
+{
+    public class TestGenerator
+    {
+        private List<Question> _allQuestions = new List<Question>();
+        private Random _random = new Random();
+        private int _nextId = 1;
+    }
+}
