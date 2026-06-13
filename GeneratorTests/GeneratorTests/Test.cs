ebe024b49ef02ec936677dbf61e33b0f789e4efa
guzer <guzer@DESKTOP-K1P33T2>
2026-06-13 19:47:50 +0300

    был создан класс Тест, в него добавлены переменные и конструктор

---
+++ GeneratorTests/GeneratorTests/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTests
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Question> Questions { get; set; }
+
+        public Test(int id, string title, List<Question> questions)
+        {
+            Id = id;
+            Title = title;
+            Questions = questions;
+        }
+    }
+}
