7ce8205ba8685b8f54ae7d069a4e9fcb2572f6e4
guzer <guzer@DESKTOP-K1P33T2>
2026-06-13 19:58:16 +0300

    в метод был добавлен блок try для обработки ошибок чтения файла, была реализована очистка списка вопросов перед загрузкой и проверка на пустой файл с выводом сообщения

--- GeneratorTests/GeneratorTests/TestGenerator.cs
+++ GeneratorTests/GeneratorTests/TestGenerator.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GeneratorTests
 {
@@ -14,8 +16,19 @@ namespace GeneratorTests
 
         public bool LoadFromFile(string path)
         {
+            try
+            {
+                _allQuestions.Clear();
+                _nextId = 1;
 
+                string[] lines = File.ReadAllLines(path);
 
-        }
+                if (lines.Length == 0)
+                {
+                    MessageBox.Show("Файл пуст. Добавьте вопросы.");
+                    return false;
+                }
+
+            }
     }
 }
