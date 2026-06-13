8ed150cd258e2e40659823d110d4d57c8f561f7d
guzer <guzer@DESKTOP-K1P33T2>
2026-06-13 21:02:41 +0300

    Добавлен метод, который сохраняет один тест в файл формата .docx. Этот метод создаёт файл с расширением .docx, записывает в него заголовок теста, затем по очереди выводит все вопросы с номерами. Если у вопроса есть варианты ответов, они тоже записываются с буквами a, b, c, d. В конце каждого вопроса добавляется строчка для ответа. После записи всех вопросов файл закрывается. Если при сохранении возникает ошибка, программа показывает сообщение и возвращает false

--- GeneratorTests/GeneratorTests/WordExporter.cs
+++ GeneratorTests/GeneratorTests/WordExporter.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace GeneratorTests
 {
     public class WordExporter
     {
+        public bool SaveToWord(string fileName, Test test)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(fileName + ".docx");
+
+                sw.WriteLine(test.Title);
+                sw.WriteLine("------------------------");
+                sw.WriteLine();
+
+                for (int i = 0; i < test.Questions.Count; i++)
+                {
+                    Question q = test.Questions[i];
+                    sw.WriteLine((i + 1) + ". " + q.Text);
+                    sw.WriteLine();
+
+                    if (q.Options.Count > 0)
+                    {
+                        for (int j = 0; j < q.Options.Count; j++)
+                        {
+                            char letter = (char)(65 + j);
+                            sw.WriteLine("   " + letter + ") " + q.Options[j]);
+                        }
+                        sw.WriteLine();
+                    }
+
+                    sw.WriteLine("   Ответ: __________");
+                    sw.WriteLine();
+                }
+
+                sw.Close();
+                MessageBox.Show("Сохранено в файл: " + fileName + ".docx");
+                return true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("Ошибка! Не удалось сохранить файл. " + ex.Message);
+                return false;
+            }
+        }
     }
 }
