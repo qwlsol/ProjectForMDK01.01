a040fcbfa68b22d9171190af0bc8f18f5b0870f6
guzer <guzer@DESKTOP-K1P33T2>
2026-06-13 20:51:14 +0300

    создан класс результат теста. В него добавлены переменные и конструктор

---
+++ GeneratorTests/GeneratorTests/TestResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTests
+{
+    public class TestResult
+    {
+        public int TestId { get; set; }
+        public int UserId { get; set; }
+        public int Score { get; set; }
+        public int MaxScore { get; set; }
+        public Dictionary<int, string> Answers { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public TestResult(int testId, int userId, int maxScore)
+        {
+            TestId = testId;
+            UserId = userId;
+            MaxScore = maxScore;
+            Score = 0;
+            Answers = new Dictionary<int, string>();
+            Timestamp = DateTime.Now;
+        }
+    }
+}
