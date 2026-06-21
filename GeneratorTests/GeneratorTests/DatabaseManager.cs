6c7843efba8649eb166cf56bb0da1fd4b8dd4b1c
guzer <guzer@DESKTOP-K1P33T2>
2026-06-21 13:43:13 +0300

    Создан класс для подключения к бд

---
+++ GeneratorTests/GeneratorTests/DatabaseManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTests
+{
+    public class DatabaseManager
+    {
+        private string _connectionString =
+            "Host=localhost;Username=postgres;Password=123;Database=generator_tests";
+    }
+}
