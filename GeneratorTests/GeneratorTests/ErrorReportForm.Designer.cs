bb9fb3078b0cfdf4f3eab609f4c095289f03a304
guzer <guzer@DESKTOP-K1P33T2>
2026-06-21 17:25:33 +0300

    Конструктор формы отчёта об ошибках принимает список ошибок. Если ошибок нет, выводит сообщение об отсутствии ошибок. Иначе формирует отчёт с заголовком, общим количеством ошибок и перечислением каждой ошибки с нумерацией. Устанавливает сформированный отчёт в текстовое поле

---
+++ GeneratorTests/GeneratorTests/ErrorReportForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace GeneratorTests
+{
+    partial class ErrorReportForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtErrors = new System.Windows.Forms.RichTextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            this.txtErrors.Location = new System.Drawing.Point(12, 12);
+            this.txtErrors.Name = "txtErrors";
+            this.txtErrors.ReadOnly = true;
+            this.txtErrors.Size = new System.Drawing.Size(600, 350);
+            this.txtErrors.TabIndex = 0;
+
+            this.btnOk.Location = new System.Drawing.Point(260, 370);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(100, 30);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "OK";
+
+            this.ClientSize = new System.Drawing.Size(624, 410);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtErrors);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ErrorReportForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Отчет об ошибках";
+            this.ResumeLayout(false);
+        }
+
+        public System.Windows.Forms.RichTextBox txtErrors;
+        public System.Windows.Forms.Button btnOk;
+    }
+}
