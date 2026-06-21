7f03e2c3ba20954bab5ae17880476e5a07342aac
guzer <guzer@DESKTOP-K1P33T2>
2026-06-21 13:19:03 +0300

    добавлен класс загрзки результата и добавлены поля

---
+++ GeneratorTests/GeneratorTests/LoadingResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorTests
+{
+    public class LoadingResult
+    {
+        public bool Success { get; set; }
+        public List<Question> LoadedQuestions { get; set; } = new List<Question>();
+        public List<string> Errors { get; set; } = new List<string>();
+        public int TotalProcessed { get; set; }
+        public int SkippedCount { get; set; }
+    }
+}
